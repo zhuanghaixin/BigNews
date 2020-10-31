0e0a5ca8d7072fb42f470cb07b22478c364c083e
zhuanghaixin <649308293@qq.com>
2020-10-31 16:52:49 +0800

    feat(login.js/loign.html): 完成登录功能

---
+++ admin/js/login.js
@@ -0,0 +1,66 @@
+$(function () {
+
+    $('.input_sub').on('click', function (e) {
+        //阻止默认行为
+        e.preventDefault()
+        const userInfo = $('.input_txt').val().trim()
+        const password = $('.input_pass').val().trim()
+        console.log(userInfo)
+        console.log(password)
+
+        //校验
+        if(userInfo=="" || password==""){
+            // alert('用户名和密码不能为空')
+            $("#myModal .modal-body").text("账号和密码不能为空!");
+            $('#myModal').modal()
+        }
+        $.ajax({
+            type:'post',
+            url:'http://localhost:8080/api/v1/admin/user/login',
+            data:{
+                username:userInfo,
+                password:password
+            },
+            success:function(res){
+                console.log(res)
+                $("#myModal .modal-body").text(res.msg);
+                $('#myModal').modal()
+                if(res.code==200){
+                    //跳转
+                    //本地存储token
+                    localStorage.setItem('token',res.token)
+                    $('#myModal').on('hidden.bs.modal', function (e) {
+                        // do something...
+                        window.location.href='./index.html'
+                    })
+                }
+
+
+            },
+            error:function(jqXHR,textStatus,errorThrown){
+
+                if(jqXHR){
+                    $("#myModal .modal-body").text('当前接口有问题');
+                    $('#myModal').modal()
+                    console.log('jqXHR...')
+                    console.log(jqXHR)
+                    console.log('jqXHR.readyState')
+                    console.log(jqXHR.readyState)
+                    console.log('jqXHR.status')
+                    console.log(jqXHR.status)
+                    console.log('jqXHR.statusText')
+                    console.log('jqXHR.statusText')
+                    console.log('jqXHR.responseText')
+                    console.log(jqXHR.responseText)
+                    console.log('textStatus...')
+                    console.log(textStatus)
+                    console.log('errorThrown...')
+                    console.log(errorThrown)
+                }
+            }
+        })
+    })
+
+
+
+})
