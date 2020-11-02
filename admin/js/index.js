168e81c59d86f8ca078b5fe6c8fa05c914438772
zhuanghaixin <649308293@qq.com>
2020-11-02 17:11:41 +0800

    feat(main.css/index.js/index.html): 完成导航功能

--- admin/js/index.js
+++ admin/js/index.js
@@ -32,39 +32,42 @@ $(function(){
         //     }
         // }
     })
-
-    //三: 首页左侧一级菜单设置点击事件
-    $("div.level01").on("click", function() {
-        //当前点击的设置一个active类,其他的兄弟移除active类.
+    //点击一级导航，显示选中状态
+    $('.sider .menu .level01').on('click',function(){
         $(this)
             .addClass("active")
             .siblings("div")
             .removeClass("active");
-        //如果你点击的是文章管理.
-        if ($(this).index() == 1) {
-            $("ul.level02").slideToggle(); //二级菜单显示就隐藏,隐藏就显示
 
-            //设置小剑尖的旋转(其实就是设置有咩有rotate0类- 本质是c3的旋转动画)
-            $(this)
-                .find("b")
-                .toggleClass("rotate0");
+    })
+    //点击一级导航，显示二级导航
+    $('.sider .menu .level01>a').on('click',function(){
+        console.log($(this).parent())
+       const $subMenu=$(this).parent().find('.level02')
+       $subMenu.slideToggle()
 
-            //默认选中第一个二级菜单.
-            $("ul.level02>li:eq(0)>a")[0].click();
-            //jQuery对象的click()事件,他只会触发js单击事件,而不会触发a标签的默认跳转事件.
-            //dom对象的click()事件,他不仅会触发js单击事件,还会触发a标签的默认跳转事件
-        }
-    });
+        //箭头旋转
+        $(this).parent()
+            .find("b")
+            .toggleClass("rotate0");
 
-    //四:首页左侧二级菜单设置点击事件
-    $("ul.level02>li").on("click", function() {
-        //当前点击的添加active类,其他的兄弟移除active类
+        //二级导航为选中状态
+        $subMenu.find('li>a').eq(0)[0].click()
+
+    })
+    //点击二级导航，显示选中状态
+    $('.sider .menu .level02>li').on('click',function(){
+        console.log(this)
         $(this)
             .addClass("active")
             .siblings("li")
             .removeClass("active");
-    });
-    //登出
+
+
+    })
+
+
+
     $(".logout").on("click", function() {
         //2.删除token,跳转到登录页面
         window.localStorage.removeItem("token");
